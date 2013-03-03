5fe56baac621bec513e0635bf87138176924e6d6
Mouse <mouse@Rodents-Montreal.ORG>
2013-03-03 18:18:36 -0500

    Add touches.sh, to help work around source-file timestamp bugs.

---
+++ touches.sh
@@ -0,0 +1,17 @@
+# This file encapsulates the state of my knowledge of what files need to
+#  be touched to avoid builds crashing because of timestamp order bugs.
+#  It is designed to be run through sh in the top-level source directory.
+touch tools/gmake/../../gnu/dist/gmake/doc/make.info
+touch tools/gmake/../../gnu/dist/gmake/doc/make.info-1
+touch tools/gmake/../../gnu/dist/gmake/doc/make.info-2
+touch tools/gmake/../../gnu/dist/gmake/doc/make.info-3
+touch tools/gmake/../../gnu/dist/gmake/doc/make.info-4
+touch tools/gmake/../../gnu/dist/gmake/doc/make.info-5
+touch tools/gmake/../../gnu/dist/gmake/doc/make.info-6
+touch tools/gmake/../../gnu/dist/gmake/doc/make.info-7
+touch tools/gmake/../../gnu/dist/gmake/doc/make.info-8
+touch tools/gmake/../../gnu/dist/gmake/doc/make.info-9
+touch tools/gmake/../../gnu/dist/gmake/doc/make.info-10
+touch tools/gmake/../../gnu/dist/gmake/doc/make.info-11
+touch dist/file/config.h.in
+touch gnu/usr.bin/binutils/ld/../../../dist/binutils/ld/configdoc.texi
