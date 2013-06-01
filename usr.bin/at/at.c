8eff912f02a9be10291de9d009f02f9245ce4ab1
Mouse <mouse@Rodents-Montreal.ORG>
2013-05-31 23:07:01 -0400

    Make atjob listings use a saner time format.

--- usr.bin/at/at.c
+++ usr.bin/at/at.c
@@ -466,7 +466,7 @@ list_jobs(void)
 
 		runtimer = 60 * (time_t)ctm;
 		runtime = *localtime(&runtimer);
-		(void)strftime(timestr, TIMESIZE, "%X %x", &runtime);
+		(void)strftime(timestr, TIMESIZE, "%Y-%m-%d %H:%M", &runtime);
 		if (first) {
 			(void)printf("%-*s  %-*s  %-*s  %s\n",
 			    (int)strlen(timestr), "Date",
